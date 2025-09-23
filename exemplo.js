df8054a9758d3e1be330eb14e9abd1b427111381
milena-hoppe <milena.sales@aluno.senai.br>
2025-09-23 15:52:44 -0300

    aula 10
    atividade

--- exemplo.js
+++ exemplo.js
@@ -18,4 +18,33 @@
 // console.log(conta1.numeroConta)
 
 //Video
-<video src="" width= ""height=""></video>
+{/* <video src="" width= ""height=""></video> */}
+
+
+//class Animal {
+  //  constructor(nome) {
+    //    this.nome = nome;
+    //}
+
+    //fazerSom() {
+      //  console.log('Som de animal');
+    //}
+//}
+
+//class Cachorro extends Animal {
+
+  //  constructor(nome) {
+    //    super(nome);
+    //}
+
+    //fazerSom() {
+      //  console.log('Au, au');
+        //return 'Au au'
+    //}
+//}
+
+//let dog = new Cachorro('Rex');
+//console.log (`O meu cahorro ${dog.nome} faz ${dog.fazerSom()}`)
+//dog.fazerSom();
+
+
